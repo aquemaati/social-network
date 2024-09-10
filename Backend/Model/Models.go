1f26804a49281d6d0f35ac93319306b20164d50a
anmarvin <minwolf.999@gmail.com>
2024-09-10 14:52:46 +0000

    Add register structure

--- Backend/Model/Models.go
+++ Backend/Model/Models.go
@@ -2,6 +2,21 @@ package model
 
 type Auth struct {
 	Id       string `json:"Id"`
-	Username string `json:"Username"`
+	Email    string `json:"Email"`
 	Password string `json:"Password"`
+}
+
+type Register struct {
+	Email           string `json:"Email"`
+	Password        string `json:"Password"`
+	ConfirmPassword string `json:"ConfirmPassword"`
+	FirstName       string `json:"FirstName"`
+	LastName        string `json:"LastName"`
+	BirthDate       string `json:"BirthDate"`
+
+	// OPTIONNAL
+	ProfilePicture any    `json:"ProfilePicture"`
+	Username       string `json:"Username"`
+	AboutMe        string `json:"AboutMe"`
+	Gender         string `json:"Gender"`
 }
